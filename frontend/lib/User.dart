c1e7ddbdb4d71cfa277159f1bc239e7ad43ace0b
Blockwatch1 <005omarshehab@gmail.com>
2025-04-23 19:36:29 +0300

    5od

---
+++ frontend/lib/User.dart
@@ -0,0 +1,37 @@
+import 'package:insta/post.dart';
+
+class User{
+ List<User> _following= [];
+ List<User> _followers= [];
+ String _username;
+ String _profilePicUrl;
+ List<Post> _posts = [];
+ List<Post> _savedPosts = [];
+ User(this._username,this._profilePicUrl);
+
+ List<Post> get savedPosts => _savedPosts;
+
+  List<Post> get posts => _posts;
+
+ String get profilePicUrl => _profilePicUrl;
+
+ List<User> get following => _following;
+
+ String get username => _username;
+
+ List<User> get followers => _followers;
+ void addFollower(User user){
+   _followers.add(user);
+ }
+ void addFollowing(User user){
+   _following.add(user);
+ }
+ void removeFollowing(String username){
+  int indexToRemove = _following.indexWhere((user){ return user.username==username;});
+  _following.removeAt(indexToRemove);
+ }
+ void removeFollower(String username){
+   int indexToRemove = _followers.indexWhere((user){ return user.username==username;});
+   _followers.removeAt(indexToRemove);
+ }
+}
--- frontend/lib/thread.dart
+++ frontend/lib/thread.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:insta/User.dart';
-class Post extends StatefulWidget {
+class Thread extends StatefulWidget {
   User user;
-  String image;
   Map<User,String>comments;
   int likeAmount;
-  Post({super.key,required this.user,required this.image,required this.comments,required this.likeAmount});
+  Thread({super.key,required this.user,required this.comments,required this.likeAmount});
   @override
-  State<Post> createState() => _PostState();
+  State<Thread> createState() => _ThreadState();
 }
 String porthavenText = "The village of Porthaven nestled beside the whispering willow creek, a place where the air always smelled faintly of brine and woodsmoke. Generations of seafaring folk had carved their lives into the craggy coastline, their days dictated by the tides and the moods of the temperamental Azure Sea. Cobblestone paths wound between houses built from weathered stone, their roofs adorned with seashells and the occasional bleached whalebone. Old Man Tiber, the village's self-proclaimed historian (though his tales often stretched further than any fishing net), claimed that Porthaven was built upon the petrified remains of a giant sea serpent, its slumbering form still influencing the strange currents offshore.\\nThe inhabitants of Porthaven were as unique as their surroundings. There was Elara, the baker whose sourdough loaves tasted inexplicably of the sea, and Finnigan, the lighthouse keeper who communicated primarily through elaborate seagull calls. Young Maeve, with her uncanny ability to find lost trinkets in the sand dunes, was often sought after by frantic tourists who'd misplaced their spectacles or souvenir spoons. And then there were the Whispering Willows themselves, lining the creek bank. Legend had it that they carried the secrets of the village, their leaves rustling with forgotten stories and prophecies that only the truly attuned could understand.\\nOne particularly blustery autumn, a peculiar event stirred the quiet rhythm of Porthaven. A collection of luminous, orb-like creatures began appearing along the shoreline at dusk. They pulsed with a soft, ethereal light, casting an otherworldly glow on the sand and the crashing waves. Some villagers were frightened, whispering of ancient sea spirits and ill omens. Others, like young Maeve, were captivated by their beauty, spending hours observing their gentle undulations. Old Man Tiber, of course, had a fantastical explanation involving bioluminescent plankton brought in by a rare southern current and the tears of a lovesick mermaid.\\n";
-class _PostState extends State<Post> {
-  void handleCommentClick(){
-    Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return CommentsPage(comments: widget.comments,);
-    },));
-  }
-
+class _ThreadState extends State<Thread> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -35,13 +28,7 @@ class _PostState extends State<Post> {
                 ],
               )),
             ),
-               PostText(text: porthavenText),
-
-               Container(
-                 width: double.infinity,
-                 child: Image.network("https://m.media-amazon.com/images/M/MV5BZTI4ZGMxN2UtODlkYS00MTBjLWE1YzctYzc3NDViMGI0ZmJmXkEyXkFqcGc@._V1_QL75_UX380_CR0,4,380,562_.jpg",fit: BoxFit.fitWidth,),
-               ),
-
+            ThreadText(text: porthavenText),
             Container(
               height: 100,
               child: Expanded(
@@ -61,10 +48,7 @@ class _PostState extends State<Post> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        GestureDetector(
-                            child: Icon(Icons.comment_rounded),
-                          onTap: handleCommentClick,
-                        ),
+                        Icon(Icons.comment_rounded),
                         Text(widget.comments.length.toString())
                       ],
                     ),
@@ -88,15 +72,15 @@ class _PostState extends State<Post> {
   }
 }
 
-class PostText extends StatefulWidget {
+class ThreadText extends StatefulWidget {
   String text;
-  PostText({super.key,required this.text});
+  ThreadText({super.key,required this.text});
 
   @override
-  State<PostText> createState() => _PostTextState();
+  State<ThreadText> createState() => _ThreadTextState();
 }
 
-class _PostTextState extends State<PostText> {
+class _ThreadTextState extends State<ThreadText> {
   bool showAll=false;
   @override
   Widget build(BuildContext context) {
@@ -125,75 +109,22 @@ class _PostTextState extends State<PostText> {
       );
     }else{
       return Container(width: double.infinity,
-        padding: EdgeInsets.only(left: 20, right: 20, bottom: 15),
-        child: Column(
-          children: [
-            Text(widget.text.substring(0,200)),
-            GestureDetector(
-              onTap: (){
-                setState(() {
-                  showAll=!showAll;
-                });
-              },
-              child: Text("Show more", style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold ),),
-            )
-          ],
-        )
+          padding: EdgeInsets.only(left: 20, right: 20, bottom: 15),
+          child: Column(
+            children: [
+              Text(widget.text.substring(0,200)),
+              GestureDetector(
+                onTap: (){
+                  setState(() {
+                    showAll=!showAll;
+                  });
+                },
+                child: Text("Show more", style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold ),),
+              )
+            ],
+          )
       );
     }
-  }
-}
-class CommentsPage extends StatefulWidget {
-  CommentsPage({super.key,required this.comments});
-  Map<User,String> comments;
-  @override
-  State<CommentsPage> createState() => _CommentsPageState();
-}
-
-class _CommentsPageState extends State<CommentsPage> {
-  List<CommentWidget> currentComments =[];
-  @override
-  Widget build(BuildContext context) {
-    widget.comments.forEach((user,comment){
-      currentComments.add(CommentWidget(user: user, comment: comment));
-    });
-    print(currentComments);
-    return ListView(
-          children: currentComments,
-    );
-  }
-}
-class CommentWidget extends StatefulWidget {
-  User user;
-  String comment;
-  CommentWidget({super.key,required this.user,required this.comment});
-
-  @override
-  State<CommentWidget> createState() => _CommentWidgetState();
-}
-
-class _CommentWidgetState extends State<CommentWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      color: Color.fromRGBO(13, 13, 13, 1),
-      margin: EdgeInsets.only(bottom: 10),
-      child: Column(
-        children: [
-          Expanded(
-              child: Row(
-                children: [
-                  SizedBox(width: 15,),
-                  CircleAvatar(backgroundImage: NetworkImage(widget.user.profilePicUrl),radius: 20,),
-                  Text(widget.user.username)
-                ],
-              )
-          ),
-          Text(widget.comment)
-        ],
-      ) ,
-    );
   }
 }
 
