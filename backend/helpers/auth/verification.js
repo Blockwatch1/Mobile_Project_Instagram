10f012d8f6a82844a4f3bf32d09e57664faacee1
marwanmoub <moubayedmarwancoding@gmail.com>
2025-04-25 18:04:37 +0300

    Fix authorization implementation

--- backend/helpers/auth/verification.js
+++ backend/helpers/auth/verification.js
@@ -3,28 +3,25 @@ import jwt from 'jsonwebtoken';
 const jwtSecret = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-
-  if (token == null) {
-    return res.status(401).json({
-      message: 'Authentication token required',
-      success: false,
-    });
+  const authHeader = req.header('Authorization');
+  if (!authHeader) {
+    console.log('Missing token');
+    return res.status(401).json({ message: 'Unauthorized: Missing Token', success: false });
   }
 
-  jwt.verify(token, jwtSecret, (err, decodedPayload) => {
-    if (err) {
-      console.error('JWT Verification Error: ', err.message);
+  const [bearer, token] = authHeader.split(' ');
 
-      return res.status(403).json({
-        message: 'Invalid or expired token',
-        success: false,
-      });
+  if (bearer !== 'Bearer' || !token) {
+    console.log('Invalid token');
+    return res.status(401).json({ message: 'Invalid token format' });
+  }
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Forbidden: Invalid Token' });
     }
 
-    req.user = decodedPayload;
-
+    req.user = user;
     next();
   });
 };
